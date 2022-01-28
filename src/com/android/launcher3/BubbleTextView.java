4bc443d23008a54b2734c4695ddbd96c0e943485
SakuraiLH <hikarilh@foxmail.com>
2022-01-29 00:37:47 +0800

    DerpLauncher: Display themed app icon all the time
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>
    Change-Id: I97e85dc4af0a2750329a733bdc746157536bd004

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -334,7 +334,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
 
     @UiThread
     protected void applyIconAndLabel(ItemInfoWithIcon info) {
-        boolean useTheme = mDisplay == DISPLAY_WORKSPACE || mDisplay == DISPLAY_FOLDER;
+        boolean useTheme = mDisplay == DISPLAY_WORKSPACE || mDisplay == DISPLAY_FOLDER || mDisplay == DISPLAY_ALL_APPS;
         FastBitmapDrawable iconDrawable = info.newIcon(getContext(), useTheme);
         mDotParams.color = IconPalette.getMutedColor(iconDrawable.getIconColor(), 0.54f);
 
