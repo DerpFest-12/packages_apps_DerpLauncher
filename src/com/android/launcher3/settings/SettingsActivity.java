8f5c5cdc7abca43530d1e8a26e9069d928865893
Joey <joey@lineageos.org>
2022-01-30 20:55:46 +0100

    DerpLauncher: categorize settings
    Change-Id: I42089e4a106f7bd7a10b5dcba68a1c92cc60cdb4
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -235,9 +235,19 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
             PreferenceScreen screen = getPreferenceScreen();
             for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                Preference preference = screen.getPreference(i);
-                if (!initPreference(preference)) {
-                    screen.removePreference(preference);
+                final Preference preference = screen.getPreference(i);
+                if (preference instanceof PreferenceCategory) {
+                    final PreferenceCategory category = (PreferenceCategory) preference;
+                    for (int j = category.getPreferenceCount() - 1; j >= 0; j--) {
+                        final Preference p = category.getPreference(j);
+                        if (!initPreference(p)) {
+                            category.removePreference(p);
+                        }
+                    }
+                } else {
+                    if (!initPreference(preference)) {
+                        screen.removePreference(preference);
+                    }
                 }
             }
 
