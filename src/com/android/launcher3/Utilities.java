AICP/packages_apps_Launcher3

QuickSpace: add option to change date format
Squash of the following commits:

add option to change date format for at a glance || https://github.com/hundeva/Lean-Launcher/commit/b4386996014e4f9761f26975625b4b983523af3d
change date formatting logic || https://github.com/hundeva/Lean-Launcher/commit/506259e8ce14b357fa456596f1dbaba21a63adba

Also, put everything related to AAG in a different category

AICP: adapt to our settings frame

Change-Id: I995ee7dde159bb14d1a3ec3f7799cd9a2db6b1d4

src/com/android/launcher3/Utilities.java
@@ -61,6 +61,7 @@ import android.os.Message;
 import android.os.PowerManager;
 import android.os.TransactionTooLargeException;
 import android.provider.Settings;
+import android.text.format.DateUtils;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -76,6 +77,7 @@ import android.view.animation.Interpolator;
 
 import androidx.core.os.BuildCompat;
 
+import com.android.launcher3.R;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.dragndrop.FolderAdaptiveIcon;
 import com.android.launcher3.graphics.GridOptionsProvider;
@@ -144,6 +146,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_NOWPLAYING = "pref_quickspace_np";
     public static final String KEY_SHOW_ALT_QUICKSPACE = "pref_show_alt_quickspace";
     public static final String KEY_SHOW_QUICKSPACE_PSONALITY = "pref_quickspace_psonality";
+    public static final String DATE_FORMAT_ATAGLANCE = "pref_date_format";
 
     private static final long WAIT_BEFORE_RESTART = 250;
 
@@ -179,10 +182,6 @@ public final class Utilities {
         ResolveInfo ri = context.getPackageManager().resolveActivity(
                 PackageManagerHelper.getStyleWallpapersIntent(context), 0);
         return ri != null;
-    }
-
-    public static boolean isQuickspaceNowPlaying(Context context) {
-        return getPrefs(context).getBoolean(KEY_SHOW_QUICKSPACE_NOWPLAYING, true);
     }
 
     /**
@@ -719,29 +718,6 @@ public final class Utilities {
         }
     }
 
-    public static String formatDateTime(Context context, long timeInMillis) {
-        try {
-            String format = "EEEE, MMM d";
-            String formattedDate;
-            DateFormat dateFormat = DateFormat.getInstanceForSkeleton(format, Locale.getDefault());
-            dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
-            formattedDate = dateFormat.format(timeInMillis);
-            return formattedDate;
-        } catch (Throwable t) {
-            Log.e(TAG, "Error formatting At A Glance date", t);
-            return DateUtils.formatDateTime(context, timeInMillis, DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH);
-        }
-
-    }
-
-    public static boolean showQSB(Context context) {
-        SharedPreferences prefs = getPrefs(context.getApplicationContext());
-        if (!LauncherAppState.getInstanceNoCreate().isSearchAppAvailable()) {
-            return false;
-        }
-        return prefs.getBoolean(KEY_SHOW_SEARCHBAR, true);
-    }
-
     public static boolean isNotificationGestureEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_NOTIFICATION_GESTURE, true);
@@ -824,5 +800,41 @@ public final class Utilities {
 
     public static boolean isQuickspacePersonalityEnabled(Context context) {
         return getPrefs(context).getBoolean(KEY_SHOW_QUICKSPACE_PSONALITY, true);
+    }
+
+    public static boolean isQuickspaceNowPlaying(Context context) {
+        return getPrefs(context).getBoolean(KEY_SHOW_QUICKSPACE_NOWPLAYING, true);
+    }
+
+    public static String getDateFormat(Context context) {
+        return getPrefs(context).getString(DATE_FORMAT_ATAGLANCE, context.getString(R.string.date_format_normal));
+    }
+
+    public static String formatDateTime(Context context, long timeInMillis) {
+        try {
+            String format = getDateFormat(context);
+            String formattedDate;
+            if (Utilities.ATLEAST_OREO) {
+                DateFormat dateFormat = DateFormat.getInstanceForSkeleton(format, Locale.getDefault());
+                dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
+                formattedDate = dateFormat.format(timeInMillis);
+            } else {
+                int flags;
+                if (format.equals(context.getString(R.string.date_format_long))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE;
+                } else if (format.equals(context.getString(R.string.date_format_normal))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                } else if (format.equals(context.getString(R.string.date_format_short))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH | DateUtils.FORMAT_ABBREV_WEEKDAY;
+                } else {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                }
+                 formattedDate = DateUtils.formatDateTime(context, timeInMillis, flags);
+            }
+            return formattedDate;
+        } catch (Throwable t) {
+            Log.e(TAG, "Error formatting At A Glance date", t);
+            return DateUtils.formatDateTime(context, timeInMillis, DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH);
+        }
     }
 }

DerpFest-12/packages_apps_DerpLauncher

src/com/android/launcher3/Utilities.java
@@ -57,6 +57,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.TransactionTooLargeException;
 import android.provider.Settings;
+import android.text.format.DateUtils;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -74,6 +75,7 @@ import android.widget.LinearLayout;
 import androidx.core.graphics.ColorUtils;
 import androidx.core.os.BuildCompat;
 
+import com.android.launcher3.R;
 import com.android.launcher3.dragndrop.FolderAdaptiveIcon;
 import com.android.launcher3.graphics.GridCustomizationsProvider;
 import com.android.launcher3.graphics.TintedDrawableSpan;
@@ -134,6 +136,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_NOWPLAYING = "pref_quickspace_np";
     public static final String KEY_SHOW_QUICKSPACE_NOWPLAYING_SHOWDATE = "pref_quickspace_np_showdate";
     public static final String KEY_SHOW_QUICKSPACE_PSONALITY = "pref_quickspace_psonality";
+    public static final String DATE_FORMAT_ATAGLANCE = "pref_date_format";
     public static final String KEY_ALLOW_WALLPAPER_ZOOM = "pref_allow_wallpaper_zoom";
     public static final String KEY_RECENT_OPACITY = "pref_recent_opacity";
     public static final String KEY_HIDE_PISSELBAR = "pref_hide_pisselbar";
@@ -197,10 +200,6 @@ public final class Utilities {
         ResolveInfo ri = context.getPackageManager().resolveActivity(
                 PackageManagerHelper.getStyleWallpapersIntent(context), 0);
         return ri != null;
-    }
-
-    public static boolean isQuickspaceNowPlaying(Context context) {
-        return getPrefs(context).getBoolean(KEY_SHOW_QUICKSPACE_NOWPLAYING, true);
     }
 
     /**
@@ -884,21 +883,6 @@ public final class Utilities {
         }
     }
 
-    public static String formatDateTime(Context context, long timeInMillis) {
-        try {
-            String format = "EEEE, MMM d";
-            String formattedDate;
-            DateFormat dateFormat = DateFormat.getInstanceForSkeleton(format, Locale.getDefault());
-            dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
-            formattedDate = dateFormat.format(timeInMillis);
-            return formattedDate;
-        } catch (Throwable t) {
-            Log.e(TAG, "Error formatting At A Glance date", t);
-            return DateUtils.formatDateTime(context, timeInMillis, DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH);
-        }
-
-    }
-
     public static boolean isDoubleTapGestureEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DT_GESTURE, true);
@@ -917,6 +901,42 @@ public final class Utilities {
     public static boolean isQuickspacePersonalityEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_SHOW_QUICKSPACE_PSONALITY, true);
+    }
+
+    public static boolean isQuickspaceNowPlaying(Context context) {
+        return getPrefs(context).getBoolean(KEY_SHOW_QUICKSPACE_NOWPLAYING, true);
+    }
+
+    public static String getDateFormat(Context context) {
+        return getPrefs(context).getString(DATE_FORMAT_ATAGLANCE, context.getString(R.string.date_format_normal));
+    }
+
+    public static String formatDateTime(Context context, long timeInMillis) {
+        try {
+            String format = getDateFormat(context);
+            String formattedDate;
+            if (Utilities.ATLEAST_P) {
+                DateFormat dateFormat = DateFormat.getInstanceForSkeleton(format, Locale.getDefault());
+                dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
+                formattedDate = dateFormat.format(timeInMillis);
+            } else {
+                int flags;
+                if (format.equals(context.getString(R.string.date_format_long))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE;
+                } else if (format.equals(context.getString(R.string.date_format_normal))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                } else if (format.equals(context.getString(R.string.date_format_short))) {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH | DateUtils.FORMAT_ABBREV_WEEKDAY;
+                } else {
+                    flags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH;
+                }
+                 formattedDate = DateUtils.formatDateTime(context, timeInMillis, flags);
+            }
+            return formattedDate;
+        } catch (Throwable t) {
+            Log.e(TAG, "Error formatting At A Glance date", t);
+            return DateUtils.formatDateTime(context, timeInMillis, DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_MONTH);
+        }
     }
 
     public static boolean canZoomWallpaper(Context context) {