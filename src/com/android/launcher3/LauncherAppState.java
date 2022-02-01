4c409d963fe0e5526057faf219d7ef0a35bf2545
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-01 09:32:58 +0700

    DerpLauncher: Update the hidden apps implementation
    After AppFilter simplification, the best way to implement this is just
    straight hook them to the particular method just like the AppFilter
    itself.

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -38,6 +38,7 @@ import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.icons.IconCache;
 import com.android.launcher3.icons.IconProvider;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.pm.InstallSessionTracker;
@@ -216,6 +217,10 @@ public class LauncherAppState {
 
     public InvariantDeviceProfile getInvariantDeviceProfile() {
         return mInvariantDeviceProfile;
+    }
+
+    public TrustDatabaseHelper getTrustData() {
+        return TrustDatabaseHelper.getInstance(mContext);
     }
 
     /**
